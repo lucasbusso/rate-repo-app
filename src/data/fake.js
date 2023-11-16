4e7a5ee2d35744ee9255f81980ce8fbd330e7733
Lucas Busso <lucasxbusso@gmail.com>
2023-11-15 23:20:28 -0300

    fake data mock

---
+++ src/data/fake.js
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    name: 'Carlos Walton',
+    phone: '(808) 336-5223',
+    email: 'mauris.rhoncus@outlook.net',
+    postalZip: 'H6C 8L0',
+    country: 'Chile',
+  },
+  {
+    name: 'Alice Salinas',
+    phone: '(382) 483-0722',
+    email: 'sapien.aenean.massa@protonmail.net',
+    postalZip: '50119',
+    country: 'Ireland',
+  },
+  {
+    name: 'Warren Bradley',
+    phone: '1-629-207-4859',
+    email: 'in@hotmail.couk',
+    postalZip: '614305',
+    country: 'South Korea',
+  },
+  {
+    name: 'Yardley Stanley',
+    phone: '(731) 600-7882',
+    email: 'condimentum@google.ca',
+    postalZip: '6911',
+    country: 'Nigeria',
+  },
+  {
+    name: 'Caldwell Cote',
+    phone: '(895) 204-6217',
+    email: 'dictum.placerat@google.ca',
+    postalZip: '7941',
+    country: 'Canada',
+  },
+]
